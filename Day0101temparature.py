8e1c512cb7c2aa09581761720ef910f70b270482
희승 <>
2023-01-12 07:44:37 +0000

    Initial commit

---
+++ Day0101temparature.py
@@ -0,0 +1,8 @@
+countdown_list=[5,4,3,2,1, "Hey!"]
+for countdown in countdown_list:
+    print(countdown)
+    print(countdown_list[3])
+    print(countdown_list[-1])
+    print('프로그램 종료')
+
+
---
+++ Day0103temparature.py
@@ -0,0 +1,12 @@
+#(100°F − 32) × 5/9 = 37.778°C
+fahrenheit = float(input('화씨 온도 : '))
+celsius = (fahrenheit - 32.0) * (5.0/9.0)
+print(f'화씨 온도 {fahrenheit}도는 섭씨 {celsius}온도 입니다')
+
+
+
+
+countdown_list=[5,4,3,2,1, "Hey!"]
+for countdown in countdown_list:
+    print(countdown)
+
